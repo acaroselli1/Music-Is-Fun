031868760985374a4a40f5b17d87ade5aa399219
Alex Caroselli <alexandersylvia9@msn.com>
2017-08-04 10:23:31 -0600

    finished project

--- itunes-controller.js
+++ itunes-controller.js
@@ -1,17 +1,44 @@
-function ItunesController(){
+function ItunesController() {
   var itunesService = new ItunesService()
   //Do Not Modify the getMusic function
-  this.getMusic = function getMusic(e){
+  this.getMusic = function getMusic(e) {
     e.preventDefault();
     var artist = e.target.artist.value;
-    itunesService.getMusicByArtist(artist).then(); //after get music by artist returns what are you doing with the objects?
+    itunesService.getMusicByArtist(artist).then(drawSongs); //after get music by artist returns what are you doing with the objects?
   }
 
   //Start coding here
 
 
+  function drawSongs(songList) {
+    var template = ''
+    var songElem = document.getElementById('song-list')
 
-
-
+    for (var i = 0; i < songList.length; i++) {
+      var song = songList[i];
+      template += `
+    
+      <div class ="col-xs-3 item">
+        <div class = "card" >
+           <img class="card-img-top"  src="${song.albumArt}" alt="card cap"> 
+           <div class="card-block">
+               <p class ="card-title song-title">${song.title}</p>
+               <p class ="artist">${song.artist}</p>
+               <p>${song.collection}</p>
+               <p>${song.price}</p>
+          </div>
+        </div>
+           <audio  id ="audio-source" src= "${song.preview}"></audio> 
+          <p> <audio controls id="audio-player"> <source id ="audio-source" src= "${song.preview}" type ="audio/mpeg"></audio></p>
+         </div>
+     
+      `
+    }
+    songElem.innerHTML = template
+  }
   
-}
+  
+ }
+  
+  
+
